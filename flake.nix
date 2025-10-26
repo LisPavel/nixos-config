7e6a35a5c11edad459581467eb05e84a0658f4e4
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2025-10-26 21:21:28 +0300

    add hardware laptop config

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        /etc/nixos/hardware-configuration.nix
+	./machines/msi-bravo-15-laptop.nix
       ];
     };
   };
