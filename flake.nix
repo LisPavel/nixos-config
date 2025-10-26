f25908d7e5e412baa6653d81c010d9d5bde24dc0
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2025-10-26 19:47:28 +0300

    move files to home from root

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        ./hardware-configuration.nix
+        /etc/nixos/hardware-configuration.nix
       ];
     };
   };
