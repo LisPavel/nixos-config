1a701d10e9cfa47db9fde702e9b7ff0d2ecea373
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2025-10-30 00:37:43 +0300

    add gaming support

--- hosts/nixos-laptop/default.nix
+++ hosts/nixos-laptop/default.nix
@@ -4,4 +4,17 @@
 {
   imports = [ ./hardware-configuration.nix ];
   networking.hostName = "nixos-laptop"; # Define your hostname.
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true; # Optional: Use Gamescope compositor
+  };
+  programs.gamemode.enable = true; # Optimizes system performance for games
+  environment.systemPackages = with pkgs; [ mangohud ];
 }
