f637e917c16dfeac5020dbfb8e0a93fc77d16a68
Pavel Lisitsyn <43790794+LisPavel@users.noreply.github.com>
2025-11-15 14:50:28 +0300

    add support for ru lang

--- users/mk/home.nix
+++ users/mk/home.nix
@@ -2,4 +2,12 @@
 { pkgs, ... }: {
   home.stateVersion = "25.05";
   home.packages = with pkgs; [ kdePackages.kate ];
+  # Russian language settings
+  home.sessionVariables = {
+    LANG = "ru_RU.UTF-8";
+    LANGUAGE = "ru:en"; # Russian primary, English fallback
+    LC_MESSAGES = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+  };
 }
